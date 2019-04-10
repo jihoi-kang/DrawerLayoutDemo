4c3350d872868f230fc2fd416d9e9ab836ac94e5
JiHoi Kang <jihoi.kang@gmail.com>
2019-04-11 03:11:12 +0900

    버튼으로 drawer 열기

--- app/src/main/java/com/example/kjh/drawerlayoutdemo/MainActivity.kt
+++ app/src/main/java/com/example/kjh/drawerlayoutdemo/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.kjh.drawerlayoutdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_open_left.setOnClickListener {
+            dl_root.openDrawer(fl_left_side)
+        }
+
+        btn_open_non_animate.setOnClickListener {
+            dl_root.openDrawer(fl_left_side, false)
+        }
+
     }
 }
